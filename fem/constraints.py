b575bd20d800cd669f22bbb7a9e79c3b5753c459
afis3 <wesmoo21@gmail.com>
2025-11-24 22:24:41 -0600

    Update constraints.py
    initial drafting, considering how i want to structure the classes that will go in here

---
+++ fem/constraints.py
@@ -0,0 +1,15 @@
+# Libraries
+import sympy as sp
+# Scripts
+from Code.types import *
+from Code.symbolic.space import Space
+from Code.symbolic.geometry import Boundary, Domain
+from Code.symbolic.math import Expression, Argument
+from Code.symbolic.math import Derivative
+
+
+class BoundaryCondition:
+
+    def __init__(
+        self,     
+        )
