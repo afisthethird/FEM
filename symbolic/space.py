953ed2a536464b8505edeb20a92cd7c154f74731
afis3 <wesmoo21@gmail.com>
2025-11-24 22:30:12 -0600

    Update space.py
    some minor edits, and subclassing of Space to force specification of physical vs. reference spaces

--- symbolic/space.py
+++ symbolic/space.py
@@ -1,5 +1,6 @@
 # Libraries
 import sympy as sp
+from abc import ABC
 from dataclasses import dataclass
 # Scripts
 from Code.types import *
@@ -28,16 +29,11 @@ class Dimension:
         return result
 
 
-class Space:
-
-    def __init__(
-        self,
-        name : NameType,
-        dims : Tuple[Dimension, ...]
-        ):
-        
-        self.name = name
-        self.dims = dims
+# Currently a dataclass but may be extended to non-Cartesian coordinate systems in the future
+@dataclass
+class Space(ABC):
+    name : NameType
+    dims : Tuple[Dimension, ...]
     
     def __repr__(self):
 
@@ -69,18 +65,37 @@ class Space:
 
         return tuple([curr_dim.sym for curr_dim in self.dims])
 
+@dataclass(repr=False)
+class PhysicalSpace(Space):
+    ref_spce : "ReferenceSpace" = None
 
-class ReferenceSpace(Space):
-
-    def __init__(
+    def create_reference_space(
         self,
-        name : NameType,
-        dims : Tuple[Dimension, ...],
-        host_spce : Space
+        name : NameType = None,
+        dims : Tuple[Dimension, ...] = None    
         ):
 
-        super().__init__(name, dims)
-        self.host_spce = host_spce
+        # Don't overwrite
+        if self.ref_spce is not None:
+            return
+        # Default reference space naming scheme
+        if name is None:
+            name = f"{self.name}_ref"
+        if dims is None:
+            dims = tuple([
+                Dimension(sp.Symbol(f"{curr_dim}_ref"))
+                for curr_dim in self.dims
+                ])
+        # Creation and storage
+        self.ref_spce = ReferenceSpace(
+            name = name,
+            dims = dims,
+            phys_spce = self
+            )
+
+@dataclass(repr=False)
+class ReferenceSpace(Space):
+    phys_spce : PhysicalSpace = None
 
 
 '''
@@ -90,41 +105,38 @@ Default Cartesian coordinate spaces
 x_dim = Dimension(sp.Symbol('x'))
 x_ref_dim = Dimension(sp.Symbol('ξ'))
 
-R1 = Space(
+R1 = PhysicalSpace(
     name = 'R1', 
     dims = (x_dim,)
     )
-R1_ref = ReferenceSpace(
-    name = 'R1_ref', 
-    dims = (x_ref_dim,),
-    host_spce = R1
+R1.create_reference_space(
+    name = 'R1_ref',
+    dims = (x_ref_dim,)
     )
 
 
 y_dim = Dimension(sp.Symbol('y'))
 y_ref_dim = Dimension(sp.Symbol('η'))
 
-R2 = Space(
+R2 = PhysicalSpace(
     name = 'R2',
     dims = (x_dim, y_dim)
     )
-R2_ref = ReferenceSpace(
+R2.create_reference_space(
     name = 'R2_ref', 
-    dims = (x_ref_dim, y_ref_dim),
-    host_spce = R2
+    dims = (x_ref_dim, y_ref_dim)
     )
 
 
 z_dim = Dimension(sp.Symbol('z'))
 z_ref_dim = Dimension(sp.Symbol('ζ'))
 
-R3 = Space(
+R3 = PhysicalSpace(
     name = 'R3', 
     dims = (x_dim, y_dim, z_dim)
     )
-R3_ref = ReferenceSpace(
+R3.create_reference_space(
     name = 'R3_ref', 
-    dims = (x_ref_dim, y_ref_dim, z_ref_dim),
-    host_spce = R3
+    dims = (x_ref_dim, y_ref_dim, z_ref_dim)
     )
 
